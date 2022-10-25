23cb87b6d6ae387a0628fb1d89b4f56514f9055c
Glorentino <chrisflorentino96@gmail.com>
2022-10-25 15:28:14 -0400

    Push to Github.com

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "maps0052ea2e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
